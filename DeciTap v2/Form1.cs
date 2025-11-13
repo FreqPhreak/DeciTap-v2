3795e2661e7ffe20f0a8ada363ec700456143192
saund <saund@VAGRANTSHAD0W>
2025-11-13 19:55:30 +0000

    GNU License Added

--- DeciTap v2/Form1.cs
+++ DeciTap v2/Form1.cs
@@ -1,3 +1,20 @@
+/*
+ *  Decitap+ - [RFID/NFC UID Scanner/Convertor, e.g. RFID Inventory Tracker]
+ *  Copyright (C) 2025  [Your Full Name]
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 using MiFare;
 using MiFare.Classic;
 using MiFare.Devices;
